3d990ce8551d14f642934955bf2c9d0df19169ca
Roman Trufanov <pingvin1308@gmail.com>
2021-10-27 20:37:47 +0300

    finish working with collections

--- Collections/Program.fs
+++ Collections/Program.fs
@@ -1,3 +1,7 @@
+let printItems items =
+    List.iter(fun x -> printf "%s;" x) items
+    printfn "\n"
+
 [<EntryPoint>]
 let main argv =
 
@@ -11,21 +15,52 @@ let main argv =
     let numbers = [ 1; 2; 3; 4 ]
 
     // another way to create list
-    let numbers = [ 
-        1 
-        2 
-        3 
-        4 
-    ]
+    let numbers = [ 1; 2; 3; 4 ]
 
-    // range of numbers
+    printfn "Work with range of numbers"
     let numbers = [ 1 .. 5 ]
-
-    let newNumbers = 0 :: numbers
-
     let otherNumbers = [ 6 .. 12 ]
+    let newNumbers = 0 :: numbers
     let newNumbers = newNumbers @ otherNumbers
+    List.iter (fun x -> printf "%i " x) newNumbers
+    printfn "\n"
+
+    printfn "Work with List module: append range"
+    let numbers = [ 1 .. 5 ]
+    let otherNumbers = [ 6 .. 12 ]
+    let newNumbers = numbers |> List.append [ 6 .. 12 ]
+    List.iter (fun x -> printf "%i " x) newNumbers
+    printfn "\n"
+
+    printfn "Work with List module: append other list"
+    let newNumbers = numbers |> List.append otherNumbers
+    List.iter (fun x -> printf "%i " x) newNumbers
+    printfn "\n"
+
+    printfn "Get items by index"
+    printfn "%i" (numbers.Item 2)
+
+    // List operations
+    let fruits = [ "melon"; "apple"; "orange";  ]
+    // Iteration:
+    printfn "Iterate fruits"
+    List.iter (fun x -> printf "%s; " x) fruits
+    printfn "\n"
+
+    // Filter:
+    printfn "Filter orange"
+    let fruits =
+        List.filter (fun x -> x <> "orange") fruits
+    printItems fruits
+
+    // Sort:
+    printfn "Sort fruits"
+    let fruits = List.sort fruits
+    printItems fruits
     
-    List.iter(fun x -> printfn "%i" x) newNumbers
+    // Search:
+    printfn "Search melon"
+    let melon = List.find (fun x -> x = "melon") fruits
+    printfn "%s" melon
 
     0
